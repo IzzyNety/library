d12d5510dbf04d9bb57358a998b42b163b688bbf
IzzyNety <izzy.nety@gmail.com>
2020-10-20 09:39:45 +0500

    динамическое создание модальных окон

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,36 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
+
+$('#trigger').click(() => $('#trigger').createModal({
+  text: {
+    title: 'Modal title',
+    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore voluptatibus quisquam at officiis quos quia numquam non, mollitia dolorum quaerat beatae a veniam facere cum omnis iste libero obcaecati commodi.'
+  },
+  btns: {
+    count: 3,
+    settings: [
+      [
+        'Close',
+        ['btn-danger', 'mr-10'],
+        true
+      ],
+      [
+        'Save changes',
+        ['btn-success'],
+        false,
+        () => {
+          alert('Данные сохранены');
+        }
+      ],
+      [
+        'Another btn',
+        ['btn-warning', 'ml-10'],
+        false,
+        () => {
+          alert('Hello World')
+        }
+      ]
+    ]
+  }
+}));
